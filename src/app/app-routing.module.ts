a84b699e3a4f7a8a53a8998689c38afc92d658ba
millerbrian454 <34964641+millerbrian454@users.noreply.github.com>
2022-07-09 14:58:58 -0400

    Added the Shows and news components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowsComponent } from './Shows/shows/shows.component';
+import { NewsComponent } from './News/news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'shows', component : ShowsComponent},
+  {path: 'news', component : NewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './static-components/footer/footer.component';
+import { ShowsComponent } from './Shows/shows/shows.component';
+import { NewsComponent } from './News/news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ShowsComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
